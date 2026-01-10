fbc1ecfc8c79382f0c330bbc412959b38fb0ff05
Ponder <ponder@dogegage.xyz>
2026-01-10 16:18:46 +0000

    first commit

---
+++ nanotap.github.io/src/App.js
@@ -0,0 +1,58 @@
+import screenshot1 from './screenshots/NanoTap1.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="headerTitle">NanoTap</h1>
+        <img src={screenshot1} width="50%" className="headerScreenshot" alt="App Screen" />
+        <button
+          className="App-link"
+          onClick={() => {
+            window.location.href = "App.js#Install";
+          }}
+        >
+          Install ⬇
+        </button>
+
+
+<section id="About">
+        <h1 className="AboutTitle">
+          About
+        </h1>
+
+        <p id="Info">
+          NanoTap is the Open-Source Appstore that allows for the installation of third-party applications on iOS without signing only using a QR Code, NFC Tag or NanoCode...
+        </p>
+</section>
+
+<section id="foss">
+  <h1>
+    NanoTap is FOSS*
+  </h1>
+
+  <p>
+    This means that it is free and open source... Forever :) <br /> Find it on GitHub at: <a href='https://github.com/nanotap/nanotap'>github.com/nanotap/nanotap</a>
+  </p>
+</section>
+
+<section id="Install">
+  <h1>
+    Install
+  </h1>
+
+  <p>
+    Install NanoTap here signed following the video: <a href='https://4950934850934895080989043509380934850934850934850934503495.pages.dev/NanoTap_1.0_1768057161.ipa'>NanoTap_1.0_1768057161.ipa</a>
+  </p>
+</section>
+<video width="640" height="358" controls>
+  <source src="https://4950934850934895080989043509380934850934850934850934503495.pages.dev/InstallingNanotapdc.mov" type="video/mp4" />
+  Your browser doesn't support video playback :(
+</video>   
+      </header>
+    </div>
+  );
+}
+
+export default App;
